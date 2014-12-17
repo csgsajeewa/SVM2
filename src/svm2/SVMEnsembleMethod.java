79fe55a37eaac18436402fbed1de374343a2663e
=csgsajeewa <csgsajeewa@gmail.com>
2014-12-17 16:28:45 +0530

    code refactroing

--- src/svm2/SVMEnsembleMethod.java
+++ src/svm2/SVMEnsembleMethod.java
@@ -1,9 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package svm2;
-
 
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.LibSVM;
@@ -12,7 +7,6 @@ import weka.core.stemmers.SnowballStemmer;
 import weka.core.tokenizers.NGramTokenizer;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
-
 import java.io.File;
 import java.util.Random;
 import weka.classifiers.meta.AdaBoostM1;
@@ -22,27 +16,33 @@ import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.SelectedTag;
-import weka.core.Tag;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.ConverterUtils;
 import weka.experiment.InstanceQuery;
 import weka.filters.AllFilter;
 
+/**
+ * SVM with ensemble methods
+ * @author chamath
+ */
 public class SVMEnsembleMethod {
 
     public static void main(String[] args) {
-        System.out.println("SVM2");
-        SVMEnsembleMethod wekaTestDB = new SVMEnsembleMethod();
+        SVMEnsembleMethod svmEns = new SVMEnsembleMethod();
         try {
-          // wekaTestDB.saveTrainingDataToFile();
-            wekaTestDB.testCrossValidataion();
+            svmEns.saveTrainingDataToFile();
+            svmEns.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    public void testCrossValidataion() throws Exception {
+    /**
+     * 
+     * @throws Exception 
+     */
+    public void run() throws Exception {
 
         System.out.println("-------------------------------------------------");
         System.out.println("Running LIBSVM");
@@ -52,12 +52,10 @@ public class SVMEnsembleMethod {
         String svmOptions = "-S 0 -K 2 -C 2 -G 0.0078125";// -W 6 1"; //-C 3 -G 0.001953125"
         svm.setOptions(weka.core.Utils.splitOptions(svmOptions));
         System.out.println("SVM Type and Keranl Type= " + svm.getSVMType() + svm.getKernelType());//1,3 best result 81%
-       
-        ////////////////////////////Normalization/////////////////
-      //  svm.setNormalize(true);
+        //svm.setNormalize(true);
         
 
-        //      load training data from .arff file
+        //load training data from .arff file
         ConverterUtils.DataSource source = new ConverterUtils.DataSource("C:\\Users\\hp\\Desktop\\SVM implementation\\arffData\\balancedTrainingDataHybrid8.arff");//"C:\\Users\\hp\\Desktop\\SVM implementation\\arffData\\balancedTrainingDataHybrid5.arff
         System.out.println("\n\nLoaded data:\n\n" + source.getDataSet());
         Instances dataFiltered = source.getDataSet();
@@ -91,54 +89,18 @@ public class SVMEnsembleMethod {
 
     }
 
-    private Instances createTestInstancesFromDB() throws Exception {
-
-        InstanceQuery query = new InstanceQuery();
-        query.setUsername("root");
-        query.setPassword("");
-        query.setQuery("SELECT content, label FROM article_ceylon_today");
-        Instances data = query.retrieveInstances();
-
-
-        FastVector attributeList = new FastVector(2);
-        Attribute a1 = new Attribute("text", (FastVector) null);
-        FastVector classVal = new FastVector();
-        classVal.addElement("crime");
-        classVal.addElement("non-crime");
-        Attribute c = new Attribute("@@class@@", classVal);
-        //add class attribute and news text
-        attributeList.addElement(a1);
-        attributeList.addElement(c);
-        Instances testData = new Instances("TestNews", attributeList, 0);
-        if (testData.classIndex() == -1) {
-            testData.setClassIndex(1);
-        }
-
-        for (int i = 0; i < data.numInstances(); i++) {
-            Instance inst = new Instance(testData.numAttributes());
-            inst.setValue(a1, data.instance(i).stringValue(0));
-            inst.setDataset(testData);
-            inst.setClassMissing();
-
-            System.out.println(inst);
-            testData.add(inst);
-        }
-        return testData;
-    }
-
     /**
-     *
+     * search to find best cost and gamma values
      * @param svm
      * @param dataFiltered
      * @throws Exception
      */
-    public void gridSearch(LibSVM svm, Instances dataFiltered) throws Exception {
+   public void gridSearch(LibSVM svm, Instances dataFiltered) throws Exception {
         GridSearch gs = new GridSearch();
         gs.setClassifier(svm);
         gs.setFilter(new AllFilter());
 
-
-
+        //setting cost search values
         gs.setXProperty("classifier.cost");
         gs.setXMin(-5);
         gs.setXMax(15);
@@ -146,6 +108,7 @@ public class SVMEnsembleMethod {
         gs.setXBase(2);
         gs.setXExpression("pow(BASE,I)");
 
+        //setting gamma search values
         gs.setYProperty("classifier.gamma");
         gs.setYMin(-15);
         gs.setYMax(3);
@@ -154,37 +117,22 @@ public class SVMEnsembleMethod {
         gs.setYExpression("pow(BASE,I)");
         //-y-property classifier.kernel.gamma -y-min -5.0 -y-max 2.0 -y-step 1.0 -y-base 10.0 -y-expression pow(BASE,I) -filter weka.filters.AllFilter -x-property classifier.nu -x-min 0.01 -x-max 1.0 -x-step 10.0 -x-base 10.0 -x-expression I -sample-size 100.0 -traversal COLUMN-WISE -log-file "C:\Program Files\Weka-3-6" -S 1 -W weka.classifiers.functions.LibSVM -- -S 2 -K 2 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.0010 -P 0.1
 
-        int EVALUATION_CC = 0;
-        int EVALUATION_RMSE = 1;
-        int EVALUATION_RRSE = 2;
-        int EVALUATION_MAE = 3;
-        int EVALUATION_RAE = 4;
-        int EVALUATION_COMBINED = 5;
-        int EVALUATION_ACC = 6;
-        int EVALUATION_KAPPA = 7;
-        Tag[] TAGS_EVALUATION = {
-            new Tag(EVALUATION_CC, "CC", "Correlation coefficient"),
-            new Tag(EVALUATION_RMSE, "RMSE", "Root mean squared error"),
-            new Tag(EVALUATION_RRSE, "RRSE", "Root relative squared error"),
-            new Tag(EVALUATION_MAE, "MAE", "Mean absolute error"),
-            new Tag(EVALUATION_RAE, "RAE", "Root absolute error"),
-            new Tag(EVALUATION_COMBINED, "COMB", "Combined = (1-abs(CC)) + RRSE + RAE"),
-            new Tag(EVALUATION_ACC, "ACC", "Accuracy"),
-            new Tag(EVALUATION_KAPPA, "KAP", "Kappa")
-        };
-        SelectedTag st = new SelectedTag(EVALUATION_ACC, TAGS_EVALUATION);
-        System.out.println(st.getTags());
+        int evaluationIndex = 6;
+        SelectedTag st;
+        st = new SelectedTag(evaluationIndex, weka.classifiers.meta.GridSearch.TAGS_EVALUATION);
         gs.setEvaluation(st);
-        //newly added
         gs.setGridIsExtendable(true);
-        //
         gs.setDebug(true);
-
         gs.buildClassifier(dataFiltered);
+
         System.out.println("Criteria " + gs.getEvaluation().getSelectedTag().getID());
-        System.out.println("&&&&&&&&&&&&" + gs.getValues());
+        System.out.println("Evaluation Summary " + gs.getValues());
     }
 
+   /**
+    * 
+    * @throws Exception 
+    */
     public void saveTrainingDataToFile() throws Exception {
         System.out.println("--------------------------------------------");
         System.out.println("Save Training Data To File");
@@ -204,27 +152,28 @@ public class SVMEnsembleMethod {
 
         //create new filter for vector transformation
         StringToWordVector filter = new StringToWordVector();
-        filter.setLowerCaseTokens(true); //ok accepted
-        filter.setOutputWordCounts(true); // ok accepted given in research papers
+        filter.setLowerCaseTokens(true); 
+        filter.setOutputWordCounts(true); 
         //Tf–idf can be successfully used for stop-words filtering in various subject fields including text summarization and classification.
-        filter.setTFTransform(true); // normalization as given in scikit
-        filter.setIDFTransform(true); // normalization as given in scikit       
-        filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR4.txt")); // stop word removal given in research paper
-        filter.setTokenizer(tokenizer); // given in research papers
-        filter.setStemmer(scnlpl); // given in research papers as stemming , here we go forward as use lemmatizer
-        // feature selection has not performed as "how to" paper say that it wont improve accuracy in SVM and our dictionary
-        // is small
+        filter.setTFTransform(true); 
+        filter.setIDFTransform(true);      
+        filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR4.txt")); 
+        filter.setTokenizer(tokenizer); 
+        filter.setStemmer(scnlpl); 
         
-        //feature reduction- similar as stop word removal , we can easily remove person names,towns etc
-        /*
-         *If you only want to have the 100 most frequent terms, you stringToWordVector.setWordsToKeep(100). Note that this will try to keep 100 words of every class. If you do not want to keep 100 words per class, stringToWordVector.setDoNotOperateOnPerClassBasis(true). You will get slightly above 100 if there are several words with the same frequency, so the 100 is just a kind of target value.
+        /**feature reduction- similar as stop word removal , we can easily 
+         * remove person names,towns etc. If you only want to have the 100 most
+         * frequent terms, you stringToWordVector.setWordsToKeep(100). Note 
+         * that this will try to keep 100 words of every class. If you do not 
+         * want to keep 100 words per class, 
+         * stringToWordVector.setDoNotOperateOnPerClassBasis(true). You will 
+         * get slightly above 100 if there are several words with the same 
+         * frequency, so the 100 is just a kind of target value.
          */
        // filter.setWordsToKeep(200); 
        // filter.setDoNotOperateOnPerClassBasis(false); 
-        ////
-
+        
         System.out.println("Stemmer Name- " + filter.getStemmer());
-
 
         InstanceQuery query = new InstanceQuery();
         query.setUsername("root");
@@ -266,27 +215,22 @@ public class SVMEnsembleMethod {
                 inst.setValue(a1, instances[i].instance(j).stringValue(0));
                 inst.setValue(c, instances[i].instance(j).stringValue(1));
                 inst.setDataset(trainingData);
-
-
                 System.out.println(inst);
                 trainingData.add(inst);
                 count++;
             }
         }
 
-
         for (int k = 0; k < numOfNewsPapers; k++) {
             System.out.println("Num of articles in " + k + " " + instances[k].numInstances());
         }
         System.out.println("Total Num of Insatances= " + count);
-
 
         // apply the StringToWordVector filter
         filter.setInputFormat(trainingData);
         Instances dataFiltered = Filter.useFilter(trainingData, filter);
         System.out.println("Number of Attributes after stop words removal- " + dataFiltered.numAttributes());
         System.out.println("\n\nFiltered data:\n\n" + dataFiltered);
-
 
         ArffSaver saver = new ArffSaver();
         saver.setInstances(dataFiltered);
