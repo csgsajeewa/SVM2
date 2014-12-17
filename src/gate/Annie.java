5e7a394514a8eb9d9d5f63627e1de868599951cf
=csgsajeewa <csgsajeewa@gmail.com>
2014-12-17 15:11:25 +0530

    modify code remove unnecessary comments and spaces and add java doc

--- src/gate/Annie.java
+++ src/gate/Annie.java
@@ -1,48 +1,48 @@
-/*
- * load annie from the saved state and then set corpus and run it.
- */
 package gate;
-import gate.*;
+
 import gate.util.GateException;
 import gate.util.persistence.PersistenceManager;
 import java.io.File;
 import java.io.IOException;
-/**
- *
- * @author hp
- */
 
+/**
+ * load ANNIE plugin from the saved state and then set corpus and run it.
+ * @author chamath
+ */
 public class Annie  {
 
   /** The Corpus Pipeline application to contain ANNIE */
   private CorpusController annieController;
 
   /**
-   * Initialise the ANNIE system. This creates a "corpus pipeline"
+   * Initialize the ANNIE system. This creates a "corpus pipeline"
    * application that can be used to run sets of documents through
    * the extraction system.
+   * 
+   * @throws GateException
+   * @throws IOException 
    */
   public void initAnnie() throws GateException, IOException {
-   
-
     // load the ANNIE application from the saved state in plugins/ANNIE
     File pluginsHome = Gate.getPluginsHome();
     File anniePlugin = new File(pluginsHome, "ANNIE");
     File annieGapp = new File(anniePlugin, "ANNIE_with_defaults.gapp");
-    annieController =(CorpusController) PersistenceManager.loadObjectFromFile(annieGapp);
-  
-    
-  } // initAnnie()
+    annieController =(CorpusController) PersistenceManager.loadObjectFromFile(annieGapp);   
+  } 
 
-  /** Tell ANNIE's controller about the corpus you want to run on */
+  /**
+   * Tell ANNIE's controller about the corpus you want to run on
+   * @param corpus 
+   */
   public void setCorpus(Corpus corpus) {
     annieController.setCorpus(corpus);
-  } // setCorpus
+  } 
 
-  /** Run ANNIE */
-  public void execute() throws GateException {
-   
-    annieController.execute();
-    
-  } // execute()
+ /**
+  * Run ANNIE
+  * @throws GateException 
+  */
+  public void execute() throws GateException {  
+    annieController.execute();    
+  } 
 }
