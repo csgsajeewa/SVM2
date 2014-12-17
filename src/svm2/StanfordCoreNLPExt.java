d1246d7a3b3ae0bc6e5f7f96b9c20dbd81d2e121
=csgsajeewa <csgsajeewa@gmail.com>
2014-12-17 15:54:48 +0530

    modify code

--- src/svm2/StanfordCoreNLPExt.java
+++ src/svm2/StanfordCoreNLPExt.java
@@ -1,14 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package svm2;
+
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import java.io.Serializable;
 import java.util.Properties;
+
 /**
  *
- * @author hp
+ * @author chamath
  */
 public class StanfordCoreNLPExt extends StanfordCoreNLP implements Serializable{
 
