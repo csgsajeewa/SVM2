4861a330bcb8d344f6911ba1fa4973661efd3612
=csgsajeewa <csgsajeewa@gmail.com>
2014-12-17 16:11:22 +0530

    code refactroing

--- src/svm2/SVMWithFeatureSelection.java
+++ src/svm2/SVMWithFeatureSelection.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package svm2;
 
 import weka.classifiers.Evaluation;
@@ -11,12 +7,10 @@ import weka.core.stemmers.SnowballStemmer;
 import weka.core.tokenizers.NGramTokenizer;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
-
 import java.io.File;
 import java.util.Random;
 import weka.attributeSelection.CfsSubsetEval;
 import weka.attributeSelection.GreedyStepwise;
-
 import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.classifiers.meta.AttributeSelectedClassifier;
@@ -32,65 +26,63 @@ import weka.experiment.InstanceQuery;
 import weka.filters.AllFilter;
 import weka.filters.supervised.attribute.AttributeSelection;
 
+/**
+ * SVM with features selected through information gain
+ *
+ * @author chamath
+ */
 public class SVMWithFeatureSelection {
 
     public static void main(String[] args) {
-        System.out.println("SVM2");
-        SVMWithFeatureSelection wekaTestDB = new SVMWithFeatureSelection();
+        System.out.println("---------------------------");
+        System.out.println("SVM with features selected through information gain");
+        System.out.println("---------------------------");
+        SVMWithFeatureSelection svm = new SVMWithFeatureSelection();
         try {
-          //  wekaTestDB.saveTrainingDataToFile();
-            wekaTestDB.testCrossValidataion();
+            svm.saveTrainingDataToFile();
+            svm.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    public void testCrossValidataion() throws Exception {
-
-
+    /**
+     *
+     * @throws Exception
+     */
+    public void run() throws Exception {
 
 //      LibSVM --> initialize the model and set SVM type and kernal type
         System.out.println("-------------------------------------------------");
         System.out.println("Running LIBSVM WIth Atrribute Selection");
         System.out.println("---------------------------------------------------");
-         
+
         LibSVM svm = new LibSVM();
         String svmOptions = "-S 0 -K 2 -C 8 -G 0.001953125 -W 10 1"; //-C 3 -G 0.00048828125"
         svm.setOptions(weka.core.Utils.splitOptions(svmOptions));
         System.out.println("SVM Type and Keranl Type= " + svm.getSVMType() + svm.getKernelType());//1,3 best result 81%
-       
-        ////////////////////////////Normalization/////////////////
         svm.setNormalize(true);
-
 
 //      load training data from .arff file
         ConverterUtils.DataSource source = new ConverterUtils.DataSource("C:\\Users\\hp\\Desktop\\SVM implementation\\arffData\\trainingData8088InstancesLemmatizer.arff");
         System.out.println("\n\nLoaded data:\n\n" + source.getDataSet());
         Instances dataFiltered = source.getDataSet();
         dataFiltered.setClassIndex(0);
-        System.out.println("number of attributes after loading from file: "+dataFiltered.numAttributes());
-       
+        System.out.println("number of attributes after loading from file: " + dataFiltered.numAttributes());
+
         //meta classifier for attribute selection - not much difference in results 
         AttributeSelectedClassifier classifier = new AttributeSelectedClassifier();
-        
-//        InfoGainAttributeEval eval = new InfoGainAttributeEval();
-//        Ranker r=new Ranker();
-//        classifier.setClassifier(svm);
-//        classifier.setEvaluator(eval);
-//        classifier.setSearch(r);
-        
-        
+
         CfsSubsetEval eval = new CfsSubsetEval();
         GreedyStepwise search = new GreedyStepwise();
         search.setSearchBackwards(true);
-        
-        
+
         classifier.setClassifier(svm);
         classifier.setEvaluator(eval);
         classifier.setSearch(search);
-        
-//        attribute selection filter- specify a threshold value to cut off certain number of features///////////////
+
+//      attribute selection filter- specify a threshold value to cut off certain number of features///////////////
         AttributeSelection filter = new AttributeSelection();
         InfoGainAttributeEval eval1 = new InfoGainAttributeEval();
         eval1.setOptions(weka.core.Utils.splitOptions("-T 0.05"));
@@ -100,71 +92,10 @@ public class SVMWithFeatureSelection {
         filter.setInputFormat(dataFiltered);
         // filter data
         Instances newData = Filter.useFilter(dataFiltered, filter);
-        System.out.println("number of attributes after attribute selection: "+newData.numAttributes());
-        ////////////////////////////////
- //       gridSearch(svm, dataFiltered);
-        Evaluation evaluation = new Evaluation(newData);
-        evaluation.crossValidateModel(svm, newData, 10, new Random(1));
-        System.out.println(evaluation.toSummaryString());
-        System.out.println(evaluation.weightedAreaUnderROC());
-        double[][] confusionMatrix = evaluation.confusionMatrix();
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(confusionMatrix[i][j] + "  ");
-
-            }
-            System.out.println();
-        }
-        System.out.println("accuracy for crime class= " + (confusionMatrix[0][0] / (confusionMatrix[0][1] + confusionMatrix[0][0])) * 100 + "%");
-        System.out.println("accuracy for other class= " + (confusionMatrix[1][1] / (confusionMatrix[1][1] + confusionMatrix[1][0])) * 100 + "%");
-
-        //get test instances and perform predictions
-//        Instances testData = createTestInstancesFromDB();
-//        Instances testDataFiltered = Filter.useFilter(testData, filter);
-//        svm.buildClassifier(dataFiltered);
-//
-//        for (int i = 0; i < testDataFiltered.numInstances(); i++) {
-//
-//            System.out.println(testData.instance(i));
-//            System.out.println(svm.classifyInstance(testDataFiltered.instance(i)));
-//            System.out.println();
-////        }
-
-    }
-
-    private Instances createTestInstancesFromDB() throws Exception {
-
-        InstanceQuery query = new InstanceQuery();
-        query.setUsername("root");
-        query.setPassword("");
-        query.setQuery("SELECT content, label FROM article_ceylon_today");
-        Instances data = query.retrieveInstances();
+        System.out.println("number of attributes after attribute selection: " + newData.numAttributes());
+        evaluate(newData, svm);
 
 
-        FastVector attributeList = new FastVector(2);
-        Attribute a1 = new Attribute("text", (FastVector) null);
-        FastVector classVal = new FastVector();
-        classVal.addElement("crime");
-        classVal.addElement("non-crime");
-        Attribute c = new Attribute("@@class@@", classVal);
-        //add class attribute and news text
-        attributeList.addElement(a1);
-        attributeList.addElement(c);
-        Instances testData = new Instances("TestNews", attributeList, 0);
-        if (testData.classIndex() == -1) {
-            testData.setClassIndex(1);
-        }
-
-        for (int i = 0; i < data.numInstances(); i++) {
-            Instance inst = new Instance(testData.numAttributes());
-            inst.setValue(a1, data.instance(i).stringValue(0));
-            inst.setDataset(testData);
-            inst.setClassMissing();
-
-            System.out.println(inst);
-            testData.add(inst);
-        }
-        return testData;
     }
 
     /**
@@ -226,6 +157,11 @@ public class SVMWithFeatureSelection {
         System.out.println("&&&&&&&&&&&&" + gs.getValues());
     }
 
+    /**
+     * save training data to file
+     *
+     * @throws Exception
+     */
     public void saveTrainingDataToFile() throws Exception {
         System.out.println("--------------------------------------------");
         System.out.println("Save Training Data To File");
@@ -235,7 +171,7 @@ public class SVMWithFeatureSelection {
         tokenizer.setNGramMinSize(1);
         tokenizer.setNGramMaxSize(1);
         tokenizer.setDelimiters("\\W");
-        
+
         //set stemmer - set english stemmer
         SnowballStemmer stemmer = new SnowballStemmer();
         stemmer.setStemmer("english");
@@ -245,27 +181,27 @@ public class SVMWithFeatureSelection {
 
         //create new filter for vector transformation
         StringToWordVector filter = new StringToWordVector();
-        filter.setLowerCaseTokens(true); //ok accepted
-        filter.setOutputWordCounts(true); // ok accepted given in research papers
+        filter.setLowerCaseTokens(true);
+        filter.setOutputWordCounts(true);
         //Tf–idf can be successfully used for stop-words filtering in various subject fields including text summarization and classification.
-        filter.setTFTransform(true); // normalization as given in scikit
-        filter.setIDFTransform(true); // normalization as given in scikit       
-        filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR4.txt")); // stop word removal given in research paper
-        filter.setTokenizer(tokenizer); // given in research papers
-        filter.setStemmer(scnlpl); // given in research papers as stemming , here we go forward as use lemmatizer
-        // feature selection has not performed as "how to" paper say that it wont improve accuracy in SVM and our dictionary
-        // is small
-        
-        //feature reduction- similar as stop word removal , we can easily remove person names,towns etc
-        /*
-         *If you only want to have the 100 most frequent terms, you stringToWordVector.setWordsToKeep(100). Note that this will try to keep 100 words of every class. If you do not want to keep 100 words per class, stringToWordVector.setDoNotOperateOnPerClassBasis(true). You will get slightly above 100 if there are several words with the same frequency, so the 100 is just a kind of target value.
+        filter.setTFTransform(true);
+        filter.setIDFTransform(true);
+        filter.setStopwords(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\StopWordsR4.txt"));
+        filter.setTokenizer(tokenizer);
+        filter.setStemmer(scnlpl);
+
+        /**
+         * feature reduction- similar as stop word removal , we can easily
+         * remove person names,towns etc. If you only want to have the 100 most
+         * frequent terms, you stringToWordVector.setWordsToKeep(100). Note that
+         * this will try to keep 100 words of every class. If you do not want to
+         * keep 100 words per class,
+         * stringToWordVector.setDoNotOperateOnPerClassBasis(true). You will get
+         * slightly above 100 if there are several words with the same
+         * frequency, so the 100 is just a kind of target value.
          */
-        filter.setWordsToKeep(200); 
-        filter.setDoNotOperateOnPerClassBasis(false); 
-        ////
-
-        System.out.println("Stemmer Name- " + filter.getStemmer());
-
+        filter.setWordsToKeep(200);
+        filter.setDoNotOperateOnPerClassBasis(false);
 
         InstanceQuery query = new InstanceQuery();
         query.setUsername("root");
@@ -284,7 +220,7 @@ public class SVMWithFeatureSelection {
 
         query.setQuery("SELECT content, label FROM article_the_island_2012 where `label` IS NOT NULL");
         instances[3] = query.retrieveInstances();
-        
+
         query.setQuery("SELECT content, label FROM article_the_island_2013 where `label` IS NOT NULL");
         instances[4] = query.retrieveInstances();
 
@@ -333,5 +269,31 @@ public class SVMWithFeatureSelection {
         saver.setInstances(dataFiltered);
         saver.setFile(new File("C:\\Users\\hp\\Desktop\\SVM implementation\\arffData\\trainingData.arff"));
         saver.writeBatch();
+    }
+
+    /**
+     * evaluate svm
+     *
+     * @param newData
+     * @param svm
+     * @throws Exception
+     */
+    private void evaluate(Instances newData, LibSVM svm) throws Exception {
+
+        Evaluation evaluation = new Evaluation(newData);
+        evaluation.crossValidateModel(svm, newData, 10, new Random(1));
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.weightedAreaUnderROC());
+        double[][] confusionMatrix = evaluation.confusionMatrix();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(confusionMatrix[i][j] + "  ");
+
+            }
+            System.out.println();
+        }
+        System.out.println("accuracy for crime class= " + (confusionMatrix[0][0] / (confusionMatrix[0][1] + confusionMatrix[0][0])) * 100 + "%");
+        System.out.println("accuracy for other class= " + (confusionMatrix[1][1] / (confusionMatrix[1][1] + confusionMatrix[1][0])) * 100 + "%");
+
     }
 }
